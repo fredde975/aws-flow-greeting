ff3caf3b653f5c2db476b9656ff74fa5af195acb
Fredrik Thernelius <fredrik.thernelius@gmail.com>
2016-05-11 21:43:45 +0200

    added a new module for the lambda function. It need to create a jar of its own.

--- hello-lambda/src/main/java/example/Hello.java
+++ hello-lambda/src/main/java/example/Hello.java
@@ -1,9 +1,9 @@
-package hellolambda;
+package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
-public class LambdaExample {
+public class Hello {
     public String myHandler(int myCount, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log("received : " + myCount);
